4d956e8f6c6378cbb9a98b0a8aa781590e8a0467
dopus <dopus@DOPUSTEAM>
2016-10-05 14:00:50 +0300

    Add project files.

---
+++ CurrencyTracker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CurrencyTracker.Startup))]
+namespace CurrencyTracker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
